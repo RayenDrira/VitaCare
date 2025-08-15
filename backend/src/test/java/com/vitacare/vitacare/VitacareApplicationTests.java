295a0435fb0945b302505d8ca2972fa0b48fe850
dammakhiba <hibadammak6@gmail.com>
2025-08-15 12:14:12 +0200

    backend

---
+++ backend/src/test/java/com/vitacare/vitacare/VitacareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitacare.vitacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitacareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
