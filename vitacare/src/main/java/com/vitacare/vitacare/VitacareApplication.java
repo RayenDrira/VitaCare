b5fdb79657caee52ab7cb8a94d9215323aba9104
dammakhiba <hibadammak6@gmail.com>
2025-08-15 12:04:09 +0200

    spring boot project

---
+++ vitacare/src/main/java/com/vitacare/vitacare/VitacareApplication.java
@@ -0,0 +1,13 @@
+package com.vitacare.vitacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitacareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitacareApplication.class, args);
+	}
+
+}
